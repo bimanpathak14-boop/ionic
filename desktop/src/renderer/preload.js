e97e32026f4c12903eecd9532de4177e28e50b15
bimanpathak14-boop <bimanpathak14@gmail.com>
2026-05-02 20:51:11 +0530

    Delete desktop/src directory

--- desktop/src/renderer/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getConfig: () => ipcRenderer.invoke('get-config'),
-  saveConfig: (config) => ipcRenderer.invoke('save-config', config),
-  pairDevice: (data) => ipcRenderer.invoke('pair-device', data),
-});
